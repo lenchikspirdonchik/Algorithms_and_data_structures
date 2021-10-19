e4d4467ae08193866f15f0fb5ebf869e56b90f00
Leonid Spiridonov <2003.leonid2003@gmail.com>
2021-10-19 13:33:47 +0300

    Update Work_3.cpp
    cool shit

--- Work_3.cpp
+++ Work_3.cpp
@@ -1,8 +1,36 @@
-#include "iostream"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+
 using namespace std;
 
 
-int main(){
-    cout << "Hello, мой мир!" <<endl;
+int main()
+{
+    setlocale(LC_ALL, "");
+    ofstream MyFile("fileText.txt");
+    string addText = "";
+    cout << "Напишите текст для добавления его в текстовый файл \n";
+    std::getline(std::cin, addText);
+    if (addText.length() < 1000)
+        MyFile << addText;
+    else cout << "Слишком длинная строка \n";
+    MyFile.close();
+
+    cout << "_____________________________________________ \n";
+
+    ifstream MyReadFile("fileText.txt");
+    char myText[1000];
+    MyReadFile.getline(myText, 1000);
+    MyReadFile.close();
+    cout << "Вывод строки с добавлением пробелов \n";
+        for (int i = 0; i < strlen(myText); i++)
+        {
+            char buff = myText[i];
+            if (buff != ' ') cout << buff << " ";
+            else cout << buff;
+        }
+
     return 0;
 }
