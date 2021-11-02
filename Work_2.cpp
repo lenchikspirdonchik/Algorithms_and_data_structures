e12a51fc2ab1137a2c495cf02877163acc5c8a8e
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-11-02 09:22:53 +0300

    shit

--- Work_2.cpp
+++ Work_2.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <valarray>
+
 //y=2x/(1+sin(a+x/2))	0,7   	5,6  	0,5
 
-double myFunction(float x, float a) {
+float myFunction(float x, float a) {
     return 2 * x / (1 + sin(a + x / 2));
 }
 
 int main()
 {
+
     int i = 1;
     float x = 0.7, xStop = 5.6, xDelta = 0.5, a = 0.0;
     std::cout << "Input a: ";
--- Work_3.cpp
+++ Work_3.cpp
@@ -1,8 +1,12 @@
 #include "iostream"
+#include <locale>
 using namespace std;
 
 
 int main(){
+    ::setlocale( LC_ALL , "russian_russia.866" );
     cout << "Hello, мой мир!" <<endl;
+    cout <<"Hello, мой мир!" << endl;
+    printf("\"Hello, мой мир!");
     return 0;
 }
